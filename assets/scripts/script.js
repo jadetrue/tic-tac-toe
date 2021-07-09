ad06af7c6e76600db18ad0262f837acf875281a6
Jade <jadetruexo@gmail.com>
2021-07-09 15:05:42 +0100

    added in a cross when target it clicked

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,8 +8,10 @@ document.getElementById("7").addEventListener("click", clickBoard);
 document.getElementById("8").addEventListener("click", clickBoard);
 document.getElementById("9").addEventListener("click", clickBoard);
 
-function clickBoard() {
-    alert("Hello you clicked the board");
-}
+const paragraph = document.createElement("p");
+const text = document.createTextNode("X")
 
-const grid = document.getElementsByClassName("grid");
+function clickBoard(event) {
+    event.target.appendChild(text);
+    event.target.style.fontSize = "50px";
+}
